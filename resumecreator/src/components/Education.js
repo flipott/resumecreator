c5a068af4429252de61ab27ae7a7635993a8a2b4
flipott <ottphillipa@gmail.com>
2022-10-26 17:52:23 -0400

    Implement ability to add several schools

--- resumecreator/src/components/Education.js
+++ resumecreator/src/components/Education.js
@@ -5,13 +5,12 @@ class Education extends Component {
         super();
 
         this.state = {
-            eduArray: {
+            eduArray: [{
                 school: '',
                 study: '',
                 startDate: '',
-                endDate: ''},
+                endDate: ''}],
             disabled: false,
-            editable: true,
             formCount: 1,
         };
     }
@@ -19,21 +18,73 @@ class Education extends Component {
     handleChange = (e) => {
         const name = e.target.name;
         const value = e.target.value;
-        const id = 0;
+        const id = e.target.id;
 
         this.setState(prevState => ({
-            eduArray: {
-                ...prevState.eduArray,
-                [name] : value
-            }
+            eduArray: prevState.eduArray.map((arr, ind) => {
+                if (ind == id) return {...arr, [name]: value}
+                return arr
+            })
         }))
     }
 
     submitHandler = (e) => {
         this.props.handleSubmit(e, this.state.eduArray);
         this.setState(prevState => ({
-            disabled: !prevState.disabled,
-            editable: !prevState.editable
+            disabled: true,
+        }))
+    }
+
+    delete = (index) => {
+        this.setState(prevState => ({
+            eduArray: prevState.eduArray.filter((arr, ind) => {
+                if (ind != index) return arr
+            }),
+            formCount: prevState.formCount - 1,
+            disabled: false
+        }))
+    }
+
+    generateInputs = () => {
+        const test = [];
+        for (let i=0; i<this.state.formCount; i++) {
+            test.push(
+                <div key={i}>
+                    <label htmlFor="school">School Name</label>
+                    <input required type="text" name="school" id={i} onChange={this.handleChange} value={this.state.eduArray[i]['school']} disabled={this.state.disabled}/>
+                    <label htmlFor="study">Area of Study</label>
+                    <input required type="text" name="study" id={i} onChange={this.handleChange} value={this.state.eduArray[i]['study']} disabled={this.state.disabled}/>
+                    <label htmlFor="startDate">Start Date</label>
+                    <input required type="date" name="startDate" id={i} onChange={this.handleChange} value={this.state.eduArray[i]['startDate']} disabled={this.state.disabled}/>
+                    <label htmlFor="endDate">End Date</label>
+                    <input required type="date" name="endDate" id={i} onChange={this.handleChange} value={this.state.eduArray[i]['endDate']} disabled={this.state.disabled}/>
+                    {this.state.formCount > 1 && (i > 0) ? <button id={i} disabled={this.state.disabled} type="button" onClick={() => this.delete(i)}>x</button> : ''}      
+                    <br></br> 
+                </div>
+            )
+        }
+        return test;
+    }
+
+    addExp = () => {
+        if (this.state.formCount === 5) {
+            console.log("You can only have a maximum of 5 entries.")
+            return false;
+        }
+        this.setState(prevState => ({
+           formCount: prevState.formCount + 1,
+           eduArray: [...prevState.eduArray, {
+            school: '',
+            study: '',
+            startDate: '',
+            endDate: ''}],
+            disabled: false
+        }))
+    }
+
+    edit = () => {
+        this.setState(prevState => ({
+            disabled: false
         }))
     }
 
@@ -41,16 +92,10 @@ class Education extends Component {
         return (
             <div>
                 <form onSubmit={(e) => this.submitHandler(e)} id="education">
-                    <label htmlFor="school">School Name</label>
-                    <input required type="text" name="school" id="school" onChange={this.handleChange} value={this.state.eduArray.school} disabled={this.state.disabled}/>
-                    <label htmlFor="study">Area of Study</label>
-                    <input required type="text" name="study" id="study" onChange={this.handleChange} value={this.state.eduArray.study} disabled={this.state.disabled}/>
-                    <label htmlFor="startDate">Start Date</label>
-                    <input required type="date" name="startDate" id="startDate" onChange={this.handleChange} value={this.state.eduArray.startDate} disabled={this.state.disabled}/>
-                    <label htmlFor="endDate">End Date</label>
-                    <input required type="date" name="endDate" id="endDate" onChange={this.handleChange} value={this.state.eduArray.endDate} disabled={this.state.disabled}/>
+                    {this.generateInputs()}
+                    <button type="button" onClick={this.addExp}>+</button>
                     <button type="submit" disabled={this.state.disabled}>Submit</button>
-                    <button disabled={this.state.editable}>Edit</button>
+                    <button disabled={!this.state.disabled} onClick={this.edit} type="button">Edit</button>
                 </form>
             </div>
         )
