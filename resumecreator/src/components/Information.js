09e6050d8d3788aad98d22e7cb7380cc7a4e0887
flipott <ottphillipa@gmail.com>
2022-10-27 02:34:38 -0400

    Added address and goal inputs

--- resumecreator/src/components/Information.js
+++ resumecreator/src/components/Information.js
@@ -10,6 +10,11 @@ class Information extends Component {
                 lastName: '',
                 email: '',
                 phone: '',
+                street: '',
+                city: '',
+                state: '',
+                zip: '',
+                goal: ''
             },
             disabled: false
         };
@@ -51,6 +56,68 @@ class Information extends Component {
                     <input required type="email" name="email" id="email" onChange={this.handleChange} value={this.state.infoArray.email} disabled={this.state.disabled}/>
                     <label htmlFor="phone">Phone Number</label>
                     <input required type="tel" name="phone" id="phone" onChange={this.handleChange} value={this.state.infoArray.phone} disabled={this.state.disabled} />
+                    <label htmlFor="street">Address</label>
+                    <input required type="text" name="street" id="street" onChange={this.handleChange} value={this.state.infoArray.street} disabled={this.state.disabled} />
+                    <label htmlFor="city">City</label>
+                    <input required type="text" name="city" id="city" onChange={this.handleChange} value={this.state.infoArray.city} disabled={this.state.disabled} />
+                    <label htmlFor="state">State</label>
+                    <select required name="state" id="state" onChange={this.handleChange} value={this.state.infoArray.state} disabled={this.state.disabled}>
+                        <option value="AL">Alabama</option>
+                        <option value="AK">Alaska</option>
+                        <option value="AZ">Arizona</option>
+                        <option value="AR">Arkansas</option>
+                        <option value="CA">California</option>
+                        <option value="CO">Colorado</option>
+                        <option value="CT">Connecticut</option>
+                        <option value="DE">Delaware</option>
+                        <option value="DC">District Of Columbia</option>
+                        <option value="FL">Florida</option>
+                        <option value="GA">Georgia</option>
+                        <option value="HI">Hawaii</option>
+                        <option value="ID">Idaho</option>
+                        <option value="IL">Illinois</option>
+                        <option value="IN">Indiana</option>
+                        <option value="IA">Iowa</option>
+                        <option value="KS">Kansas</option>
+                        <option value="KY">Kentucky</option>
+                        <option value="LA">Louisiana</option>
+                        <option value="ME">Maine</option>
+                        <option value="MD">Maryland</option>
+                        <option value="MA">Massachusetts</option>
+                        <option value="MI">Michigan</option>
+                        <option value="MN">Minnesota</option>
+                        <option value="MS">Mississippi</option>
+                        <option value="MO">Missouri</option>
+                        <option value="MT">Montana</option>
+                        <option value="NE">Nebraska</option>
+                        <option value="NV">Nevada</option>
+                        <option value="NH">New Hampshire</option>
+                        <option value="NJ">New Jersey</option>
+                        <option value="NM">New Mexico</option>
+                        <option value="NY">New York</option>
+                        <option value="NC">North Carolina</option>
+                        <option value="ND">North Dakota</option>
+                        <option value="OH">Ohio</option>
+                        <option value="OK">Oklahoma</option>
+                        <option value="OR">Oregon</option>
+                        <option value="PA">Pennsylvania</option>
+                        <option value="RI">Rhode Island</option>
+                        <option value="SC">South Carolina</option>
+                        <option value="SD">South Dakota</option>
+                        <option value="TN">Tennessee</option>
+                        <option value="TX">Texas</option>
+                        <option value="UT">Utah</option>
+                        <option value="VT">Vermont</option>
+                        <option value="VA">Virginia</option>
+                        <option value="WA">Washington</option>
+                        <option value="WV">West Virginia</option>
+                        <option value="WI">Wisconsin</option>
+                        <option value="WY">Wyoming</option>
+                    </select>
+                    <label htmlFor="zip">Zip Code</label>
+                    <input required type="text" name="zip" id="zip" onChange={this.handleChange} value={this.state.infoArray.zip} disabled={this.state.disabled} />
+                    <label htmlFor="goal">Goal / Mission Statement</label>
+                    <textarea required name="goal" id="goal" onChange={this.handleChange} value={this.state.infoArray.goal} disabled={this.state.disabled} />
                     <button type="submit" disabled={this.state.disabled}>Submit</button>
                     <button disabled={!this.state.disabled} onClick={this.edit} type="button">Edit</button>
                 </form>
