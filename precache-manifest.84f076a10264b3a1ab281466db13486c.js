5e359f98e87a0838fd117092a3239135bda1c920
flipott <ottphillipa@gmail.com>
2023-03-21 21:21:16 -0400

    Updates

---
+++ precache-manifest.84f076a10264b3a1ab281466db13486c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a07ac94c3d3fedb2fa1",
+    "url": "/resumecreator/static/css/main.9a4f573b.chunk.css"
+  },
+  {
+    "revision": "3a07ac94c3d3fedb2fa1",
+    "url": "/resumecreator/static/js/main.8b19b6ca.chunk.js"
+  },
+  {
+    "revision": "27a6d380a141a1273fe8",
+    "url": "/resumecreator/static/js/runtime~main.cb45c194.js"
+  },
+  {
+    "revision": "09b043331dbddc61ab24",
+    "url": "/resumecreator/static/js/2.2805b468.chunk.js"
+  },
+  {
+    "revision": "094f7d9a3e6fa6497acd02062885aa69",
+    "url": "/resumecreator/index.html"
+  }
+];
